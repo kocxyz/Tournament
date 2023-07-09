25ed1ce0fb576093912d30c3ee918d2925bd7a54
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-07-09 18:42:58 +0200

    refactor(web)!: Use Tournament ID instead of `ManagerId`

--- apps/web/src/app/tournament/[id]/page.tsx
+++ apps/web/src/app/tournament/[id]/page.tsx
@@ -1,8 +1,21 @@
 import BracketsViewers from '@/components/Brackets-Viewer';
 import { manager } from 'brackets';
+import { prisma } from 'database';
 
-export default async function TournamentDetailsPage({ params: { id } }: { params: { id: string } }) {
-  const data = await manager.get.tournamentData(parseInt(id));
+export default async function TournamentDetailsPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const tournament = await prisma.tournament.findFirst({
+    where: { id },
+  });
+
+  if (tournament === null) {
+    return <div />;
+  }
+
+  const data = await manager.get.tournamentData(tournament.managerTournamentId);
   return (
     <div className="flex-1 flex justify-center items-center">
       <BracketsViewers data={data} />
