66a481d5c46bdc121a7eff51410d8ff9479093c9
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-07-09 18:41:47 +0200

    feat(web): Make Navbar link to `/`

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 export default function Navbar() {
   return (
     <div className="navbar bg-base-100 px-12">
-      <a className="normal-case text-xl">Brawler Stats</a>
+      <a className="normal-case text-xl" href="/">
+        Brawler
+      </a>
     </div>
   );
 }
