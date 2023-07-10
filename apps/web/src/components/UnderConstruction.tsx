4148bf8494181198cc959f0d26802690dc2db151
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-07-10 15:15:54 +0200

    feat: Add disclaimer in UnderConstruction

--- apps/web/src/components/UnderConstruction.tsx
+++ apps/web/src/components/UnderConstruction.tsx
@@ -14,7 +14,11 @@ export default function UnderConstructionAlert() {
           d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
         />
       </svg>
-      <span>This page is currently under construction!</span>
+      <span>
+        This page is currently under construction!
+        <br />
+        You may still encounter some bugs that need to be squished.
+      </span>
     </div>
   );
 }
