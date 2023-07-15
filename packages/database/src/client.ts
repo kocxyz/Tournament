f170e729b2d1fa9473e8e657254a0488739989a5
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-07-15 15:58:39 +0200

    fix(web): Auto-Refresh caused new prisma connections

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,6 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
-prisma.$connect();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export { prisma };
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
